9a9bb9f7ba97da3b71863cd6374ba877603086a0
Divya Sri <divyasridurga.t@engro.io-at-491938176112>
2024-08-17 17:41:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-let heading = React.createElement(
-  "h1",
-  { className: "heading_1", id: "heading_container" },
-  "hello guys !!!!!!!!"
-);
-let h2tag = React.createElement("h2", {}, "heading 2 tag");
-let divTag = React.createElement("div", { class: "container" }, [
-  heading,
-  h2tag,
-]);
+// approach-1
+// let heading = React.createElement(
+//   "h1",
+//   { className: "heading_1", id: "heading_container" },
+//   "hello guys !!!!!!!!"
+// );
+// let h2tag = React.createElement("h2", {}, "heading 2 tag");
+// let divTag = React.createElement("div", { class: "container" }, [
+//   heading,
+//   h2tag,
+// ]);
+
+// approach -2
+{/* <div class="parent">
+  <div class="child">
+    <h2>H2 tag</h2>
+  </div>
+</div> */}
+
+let x= React.createElement("div",{className:"parent"}, 
+  React.createElement("div",{className:"child", key:"child"},[ React.createElement("h2", {key:"h2"},"H2 tag with nesting approach"), React.createElement("h3",{key:"h3"}, "h3 tag for sibling element reference")]
+)
+)
+
 let root = ReactDOM.createRoot(document.getElementById("route")); // manipulating reactDOM to inject the content inside div with id route
 // ReactDOM.render(divTag, root)  this is no longer supported in 18
-root.render(divTag);
+root.render(x);

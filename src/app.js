820b075506972b067d0f2d88e7f46684b94ccca4
Divya Sri <divyasridurga.t@engro.io-at-491938176112>
2024-09-18 23:08:25 +0530

    routing initial commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function Applayout() {
   return (
@@ -11,8 +12,24 @@ function Applayout() {
     </div>
   );
 }
+
+let appRouter= createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+  },
+  {
+    path:'/about',
+    element:<h1>this is about page !!!!</h1>
+  },
+  {
+    path:'/:section',
+    element:<h1>dynamic route</h1>
+  }
+]);
+
 // babel is the one which converts the functions and html elements to react create element and then renders to DOM
 
 let root = ReactDOM.createRoot(document.getElementById("root")); // manipulating reactDOM to inject the content inside div with id route
 // ReactDOM.render(divTag, root)  this is no longer supported in 18
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter} />);
